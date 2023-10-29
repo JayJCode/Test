53426fb9d1feff8b1d1d5b2959d93faf106469ee
Jan Janowicz <JayJCode@gmail.com>
2023-10-29 11:13:46 +0100

    Zadanie 15, z wykorzystaniem kodu z wprowadzenia

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int numer_one, numer_two;
+   scanf(%d %d, &number_one, &number_two);
+   printf("Suma liczb wynosi: %d\n", number_one + number_two);
+   return 0;
+}
